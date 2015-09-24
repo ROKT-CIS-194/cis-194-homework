1e14d72fa6f552a33d8256351e9a3585f57cc7d7
Ben Sinclair <ben@typius.com>
2015-09-24 21:41:06 +1000

    Run tests in a slightly more informative way

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,14 @@ module Main where
 
 import CIS194.Testing
 import qualified CIS194.Template.HW01Tests as HW01Tests
+import Text.Printf
+
+doTests :: String -> [Test] -> IO ()
+doTests name tests = do
+  let failures = runTests tests
+  printf "%s: %d tests, %d failures\n" name (length tests) (length failures)
+  mapM_ (printf "  %s\n" . show :: Failure -> IO ()) failures
 
 main :: IO ()
 main = do
-  mapM_ print (runTests HW01Tests.allTests)
+  doTests "HW01" HW01Tests.allTests
