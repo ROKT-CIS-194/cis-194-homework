965b1641d9deee2e1b6e63c37b3fb8c2a4d60cc8
Ben Sinclair <ben.d.sinclair@gmail.com>
2015-09-24 23:46:07 +1000

    BenS: all but towers of hanoi done

--- src/CIS194/BenS/HW01.hs
+++ src/CIS194/BenS/HW01.hs
@@ -5,35 +5,42 @@ module CIS194.BenS.HW01 where
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit n = n `mod` 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = undefined
+dropLastDigit n = n `div` 10
 
 -- Exercise 2 -----------------------------------------
 
 toRevDigits :: Integer -> [Integer]
-toRevDigits = undefined
+toRevDigits n
+  | n < 1     = []
+  | n < 10    = [n]
+  | otherwise = lastDigit n : toRevDigits (dropLastDigit n)
 
 -- Exercise 3 -----------------------------------------
 
 -- Double every second number in a list starting on the left.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther xs = interleave xs (map (*2) xs)
+  where
+    interleave [] bs = bs
+    interleave as [] = as
+    interleave (a:as) (_:bs) = a:interleave bs as
 
 -- Exercise 4 -----------------------------------------
 
 -- Calculate the sum of all the digits in every Integer.
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
-
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toRevDigits x) + sumDigits xs
 
 -- Exercise 5 -----------------------------------------
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = undefined
+luhn = (== 0) . lastDigit . sumDigits . doubleEveryOther . toRevDigits
 
 -- Exercise 6 -----------------------------------------
 
