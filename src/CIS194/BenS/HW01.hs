8cafe23f2f1a7253d8ddb7f939e31902e8460a36
Ben Sinclair <ben.d.sinclair@gmail.com>
2015-09-24 23:40:05 +1000

    BenS: towers of hanoi

--- src/CIS194/BenS/HW01.hs
+++ src/CIS194/BenS/HW01.hs
@@ -49,4 +49,5 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n-1) a c b ++ [(a, c)] ++ hanoi (n-1) b a c
