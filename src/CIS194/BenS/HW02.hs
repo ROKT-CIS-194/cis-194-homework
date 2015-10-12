7e8d906c0e5ef40c218d30a75e86643edf2dc5ea
Ben Sinclair <ben.sinclair@rokt.com>
2015-10-12 12:04:26 +1100

    BenS' HW02

--- src/CIS194/BenS/HW02.hs
+++ src/CIS194/BenS/HW02.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 module CIS194.BenS.HW02 where
 
+import Data.Bool
+
 -- Mastermind -----------------------------------------
 
 -- A peg can be one of six colors
@@ -23,43 +25,62 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
-exactMatches = undefined
+exactMatches xs ys = sum . map (bool 0 1) $ zipWith (==) xs ys
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors = foldr (zipWith (+)) [0, 0, 0, 0, 0, 0] . map code
+  where
+    code Red    = [1, 0, 0, 0, 0, 0]
+    code Green  = [0, 1, 0, 0, 0, 0]
+    code Blue   = [0, 0, 1, 0, 0, 0]
+    code Yellow = [0, 0, 0, 1, 0, 0]
+    code Orange = [0, 0, 0, 0, 1, 0]
+    code Purple = [0, 0, 0, 0, 0, 1]
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches secret guess =
+  sum (zipWith min (countColors secret) (countColors guess))
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove secret guess = Move guess exact nonexact
+  where
+    exact = exactMatches secret guess
+    nonexact = matches secret guess - exact
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move guess exact nonexact) code =
+  exact == exact' && nonexact == nonexact'
+  where
+    exact' = exactMatches code guess
+    nonexact' = matches code guess - exact'
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes move = filter (isConsistent move)
 
 -- Exercise 6 -----------------------------------------
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes 1 = map (:[]) colors
+allCodes n = concatMap (\cs -> map (\c -> c:cs) colors) (allCodes (n-1))
 
 -- Exercise 7 -----------------------------------------
 
 solve :: Code -> [Move]
-solve = undefined
+solve secret = reverse $ go [] (allCodes 4)
+  where
+    go ms [] = ms
+    go ms (c:cs) = go (m:ms) (filterCodes m cs) where m = getMove secret c
 
 -- Bonus ----------------------------------------------
 
