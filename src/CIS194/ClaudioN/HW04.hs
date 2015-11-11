dbdb1a06057b3bcae2c07491f407ba821f0f0f7b
Claudio Natoli <cnatoli@users.noreply.github.com>
2015-11-11 21:08:42 +1100

    Claudio - minor refinements and areas for improvement

--- src/CIS194/ClaudioN/HW04.hs
+++ src/CIS194/ClaudioN/HW04.hs
@@ -4,6 +4,7 @@ module CIS194.ClaudioN.HW04 where
 
 import Data.List
 
+-- FIXME: adopt smart constructor, to canonicalize representaton (i.e. remove leading zeros)
 newtype Poly a = P [a]
 
 -- Exercise 1 -----------------------------------------
@@ -13,24 +14,18 @@ x = P [0, 1]
 
 -- Exercise 2 ----------------------------------------
 
--- instance (Num a, Eq a) => Eq (Poly a) where
---     (P p1) == (P p2) = eq p1 p2
---       where eq (a:as) (b:bs) = (a == b) && eq as bs
---             eq as [] = allzeros as
---             eq [] bs = allzeros bs
---             allzeros l = null $ dropWhile (== 0) l
-
 instance (Num a, Eq a) => Eq (Poly a) where
     (P p1) == (P p2) = dropTerms p1 == dropTerms p2
       where dropTerms = dropWhile (== 0) . reverse
 
 -- Exercise 3 -----------------------------------------
 
+-- FIXME: pattern matching on terms would resolve need for special handling of 0 cases
 instance (Num a, Eq a, Show a) => Show (Poly a) where
     show (P[]) = "0" -- FIXME: when all 0?
     show (P[0]) = "0"
     show (P cs) = intercalate " + " $ reverse $ terms where
-      terms = filter (not . null) $ map showTerm $ zip cs [0..]
+      terms = filter (not . null) $ map showTerm $ zip cs [(0::Integer)..]
       showTerm (c, e)
         | c == 0 = ""
         | e == 0 = show c
@@ -44,10 +39,6 @@ instance (Num a, Eq a, Show a) => Show (Poly a) where
 -- Exercise 4 -----------------------------------------
 
 plus :: Num a => Poly a -> Poly a -> Poly a
--- plus (P p1) (P p2) = P $ zipWith (+) p1 p2 ++ rest p1 p2
---   where rest (_:as) (_:bs) = rest as bs
---         rest as [] = as
---         rest [] bs = bs
 plus (P p1) (P p2) = P $ add p1 p2
   where add (a:as) (b:bs) = (a+b) : add as bs
         add as [] = as
@@ -65,7 +56,7 @@ times (P p1) (P p2) = sum $ mul p1 p2
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate        = (* P [-1])  -- FIXME: normalize?
+    negate        = (* P [-1])
     fromInteger a = P [fromInteger a]
     -- No meaningful definitions exist
     abs    = undefined
@@ -73,8 +64,9 @@ instance Num a => Num (Poly a) where
 
 -- Exercise 7 -----------------------------------------
 
+-- FIXME: iterate approch is nicer
 applyP :: Num a => Poly a -> a -> a
-applyP (P cs) a = sum $ map (\(c,e) -> c * a^e) $ zip cs [0..]
+applyP (P cs) a = sum $ map (\(c,e) -> c * a^e) $ zip cs [(0::Integer)..]
 
 -- Exercise 8 -----------------------------------------
 
