db4bfae00301b87717e1a597f390691034cf0439
Ben Sinclair <ben.d.sinclair@gmail.com>
2016-01-24 22:43:54 +1100

    Add lectures up to Week 10

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,21 @@
 module Main where
 
-import qualified CIS194.Template.HW01Tests as HW01Tests
-import qualified CIS194.Template.HW02Tests as HW02Tests
-import qualified CIS194.Template.HW03Tests as HW03Tests
+import qualified CIS194.Template.HW01Tests       as HW01Tests
+import qualified CIS194.Template.HW02Tests       as HW02Tests
+import qualified CIS194.Template.HW03Tests       as HW03Tests
 import           CIS194.Testing
 import           Text.Printf
 
-import CIS194.Lectures.L01Intro
-import CIS194.Lectures.L02Lists
-import CIS194.Lectures.L03ADTs
-import CIS194.Lectures.L04TypeClasses
-import CIS194.Lectures.L05IO
+import           CIS194.Lectures.L01Intro
+import           CIS194.Lectures.L02Lists
+import           CIS194.Lectures.L03ADTs
+import           CIS194.Lectures.L04TypeClasses
+import           CIS194.Lectures.L05IO
+import           CIS194.Lectures.L06Laziness     hiding (main)
+import           CIS194.Lectures.L07Monads
+import           CIS194.Lectures.L08MonadsII
+import           CIS194.Lectures.L09Testing
+import           CIS194.Lectures.L10TypeWizardry
 
 doTests :: String -> [Test] -> IO ()
 doTests name tests = do
