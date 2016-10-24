7cf4ac8bff40c8888e1806c3bc925ff8f1b73375
Claudio Natoli <cnatoli@users.noreply.github.com>
2016-10-24 23:07:15 +1100

    ClaudioN - Week1 + Week2

--- 2016/src/CIS194/Week1.hs
+++ 2016/src/CIS194/Week1.hs
@@ -1,28 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module CIS194.Week1 where
-
 import CodeWorld
+
+main :: IO ()
+main = exercise1
 
 -- TRAFFIC LIGHTS
 
-botCircle :: Color -> Picture
-botCircle c = colored c (translated 0 (-1.5) (solidCircle 1))
-
-topCircle :: Color -> Picture
-topCircle c = colored c (translated 0   1.5  (solidCircle 1))
+trafficLight :: Color -> Double ->  Picture
+trafficLight c y = colored c (translated 0 y (solidCircle 1))
 
 frame :: Picture
-frame = rectangle 2.5 5.5
+frame = rectangle 2.5 7.5
 
-trafficLight :: Bool -> Picture
-trafficLight True  = botCircle green & topCircle black & frame
-trafficLight False = botCircle black & topCircle red   & frame
+lightColor :: Bool -> Color -> Color
+lightColor True c = c
+lightColor False _ = black
+
+trafficLights :: Bool -> Bool -> Bool -> Picture
+trafficLights r a g =
+  frame &
+  trafficLight (lightColor r red)    2.5 &
+  trafficLight (lightColor a orange) 0   &
+  trafficLight (lightColor g green) (-2.5)
 
 trafficController :: Double -> Picture
 trafficController t
-  | round (t/3) `mod` 2 == 0 = trafficLight True
-  | otherwise                = trafficLight False
+  | t' <= 1 = trafficLights False False True
+  | t' <= 2 = trafficLights False True False
+  | t' <= 4 = trafficLights True False False
+  | otherwise = trafficLights True True False
+  where t' = (round t `mod` 6)
 
 exercise1 :: IO ()
 exercise1 = animationOf trafficController
@@ -30,31 +38,48 @@ exercise1 = animationOf trafficController
 
 -- BLOSSOMS
 
-tree :: Int -> Picture
-tree 0 = blank
-tree n = path [(0,0),(0,1)] & translated 0 1 (
-  rotated (pi/10) (tree (n-1)) & rotated (- pi/10) (tree (n-1)))
+tree :: Picture -> Int -> Picture
+tree b 0 = b
+tree b n = path [(0,0),(0,1)] &
+           translated 0 1 (rotated (pi/10) (tree b (n-1)) &
+                           rotated (- pi/10) (tree b (n-1)))
+
+blossom :: Double -> Picture
+blossom t = colored pink (solidCircle s)
+  where s = case () of
+              _ | t <= 10 -> t/30
+                | otherwise -> (1/3)
+
+treeAnimation :: Double -> Picture
+treeAnimation t = tree (blossom t) 8
 
 exercise2 :: IO ()
-exercise2 = drawingOf (tree 5)
+exercise2 = animationOf treeAnimation
 
 
 -- SOKOBAN
 
+square :: Picture
+square = (solidRectangle 1 1)
+
 wall :: Picture
-wall = blank
+wall =  colored (gray 0.5) square
 
 ground :: Picture
-ground = blank
+ground = colored yellow square
 
 storage :: Picture
-storage = blank
+storage = solidCircle 0.25 & ground
 
 box :: Picture
-box = blank
+box = colored orange square
 
 drawTile :: Int -> Picture
-drawTile i = blank
+drawTile 1 = wall
+drawTile 2 = ground
+drawTile 3 = storage
+drawTile 4 = box
+drawTile _ = blank
 
 maze :: Int -> Int -> Int
 maze x y
@@ -65,8 +90,11 @@ maze x y
   | x >= -2 && y == 0        = 4
   | otherwise                = 2
 
+drawTileAt :: Int -> Int -> Picture
+drawTileAt x y = translated (fromIntegral x) (fromIntegral y) $ drawTile (maze x y)
+
 pictureOfMaze :: Picture
-pictureOfMaze = blank
+pictureOfMaze = foldr (&) blank $ map (\(x,y) -> drawTileAt x y) [(x,y) | x <- [-10..10], y <- [-10..10]]
 
 exercise3 :: IO ()
 exercise3 = drawingOf pictureOfMaze
--- 2016/src/CIS194/Week2.hs
+++ 2016/src/CIS194/Week2.hs
@@ -1,88 +1,133 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module CIS194.Week2 where
-
 import CodeWorld
+
+main :: IO ()
+main = exercise1
 
 -- LAST WEEK
 -- Replace with your implementation from last week.
-
 data Coord = C Int Int deriving (Eq, Ord, Show)
 
 data Tile = Wall | Ground | Storage | Box | Blank deriving (Eq, Show)
 
+square :: Picture
+square = (solidRectangle 1 1)
+
 wall :: Picture
-wall = blank
+wall =  colored (gray 0.5) square
 
 ground :: Picture
-ground = blank
+ground = colored yellow square
 
 storage :: Picture
-storage = blank
+storage = solidCircle 0.25 & ground
 
 box :: Picture
-box = blank
+box = colored orange square
 
 drawTile :: Tile -> Picture
+drawTile Wall    = wall
+drawTile Ground  = ground
+drawTile Storage = storage
+drawTile Box = box
 drawTile _ = blank
 
 maze :: Coord -> Tile
 maze (C x y)
-  | abs x > 4  || abs y > 4  = Blank
-  | abs x == 4 || abs y == 4 = Wall
+  | abs x > 10  || abs y > 10  = Blank
+  | abs x == 10 || abs y == 10 = Wall
   | x ==  2 && y <= 0        = Wall
   | x ==  3 && y <= 0        = Storage
   | x >= -2 && y == 0        = Box
   | otherwise                = Ground
 
+drawAt :: Coord -> Picture -> Picture
+drawAt (C x y) p = translated (fromIntegral x) (fromIntegral y) p
+
+drawTileAt :: Int -> Int -> Picture
+drawTileAt x y = drawAt (C x y) $ drawTile (maze (C x y))
+
 pictureOfMaze :: Picture
-pictureOfMaze = blank
+pictureOfMaze = foldr (&) blank $ map (\(x,y) -> drawTileAt x y) [(x,y) | x <- [-10..10], y <- [-10..10]]
 
 
 -- #1 MOVEMENT
+data Direction = U | D | L | R
 
-type World = ()
+type World = Coord
+
+moveIn :: Direction -> Coord -> Coord
+moveIn U (C x y) = C  x   (y+1)
+moveIn D (C x y) = C  x   (y-1)
+moveIn L (C x y) = C (x-1) y
+moveIn R (C x y) = C (x+1) y
+
+moveIfAllowed :: Direction -> Coord -> Coord
+moveIfAllowed d c
+  | allowed (maze c') = c'
+  | otherwise = c
+  where c' = moveIn d c
+        allowed t
+          | (t == Ground || t == Storage) = True
+          | otherwise = False
 
 player :: Picture
-player = blank
+player = solidCircle 0.5
+
+stepInput :: Event -> World -> World
+stepInput (KeyPress k) c
+  | k == "Up"    = moveIfAllowed U c
+  | k == "Down"  = moveIfAllowed D c
+  | k == "Left"  = moveIfAllowed L c
+  | k == "Right" = moveIfAllowed R c
+stepInput _ w = w
 
 exercise1 :: IO ()
 exercise1 = interactionOf world0 stepTime stepInput output
   where
     world0 :: World
-    world0 = ()
+    world0 = C 0 1
 
     stepTime :: Double -> World -> World
     stepTime _ = id
 
-    stepInput :: Event -> World -> World
-    stepInput _ = id
-
     output :: World -> Picture
-    output _ = blank
+    output c = drawAt c player & pictureOfMaze
 
 
 -- #2 LOOK THE RIGHT WAY
 
-data Direction = U | D | L | R deriving (Eq, Show)
+data World2 = World2 Coord Direction
+
+arrow :: Picture
+arrow = path [(-0.5,0),(0.5,0)] & path [(0.5,0),(0.3,0.3)] & path [(0.5,0),(0.3,-0.3)]
 
 player2 :: Direction -> Picture
-player2 _ = blank
+player2 U = rotated (pi/2) arrow
+player2 D = rotated (3*pi/2) arrow
+player2 L = rotated (pi) arrow
+player2 R = rotated 0 arrow
+
+stepInput2 :: Event -> World2 -> World2
+stepInput2 (KeyPress k) (World2 c _)
+  | k == "Up"    = World2 (moveIfAllowed U c) U
+  | k == "Down"  = World2 (moveIfAllowed D c) D
+  | k == "Left"  = World2 (moveIfAllowed L c) L
+  | k == "Right" = World2 (moveIfAllowed R c) R
+stepInput2 _ w = w
 
 exercise2 :: IO ()
-exercise2 = interactionOf world0 stepTime stepInput output
+exercise2 = interactionOf world0 stepTime stepInput2 output
   where
-    world0 :: World
-    world0 = ()
+    world0 :: World2
+    world0 = World2 (C 0 1) R
 
-    stepTime :: Double -> World -> World
+    stepTime :: Double -> World2 -> World2
     stepTime _ = id
 
-    stepInput :: Event -> World -> World
-    stepInput _ = id
-
-    output :: World -> Picture
-    output _ = blank
+    output :: World2 -> Picture
+    output (World2 c d) = drawAt c (player2 d) & pictureOfMaze
 
 
 -- #3 RESET!
@@ -93,23 +138,23 @@ resetableInteractionOf
   -> (Event -> world -> world)
   -> (world -> Picture)
   -> IO ()
-resetableInteractionOf _ _ _ _ =
-  return ()
+resetableInteractionOf world0 stepTime stepInput output
+  = interactionOf world0 stepTime stepInput' output
+  where stepInput' (KeyPress k) _ | k == "Esc" = world0
+        stepInput' e s = stepInput e s
+
 
 exercise3 :: IO ()
-exercise3 = resetableInteractionOf world0 stepTime stepInput output
+exercise3 = resetableInteractionOf world0 stepTime stepInput2 output
   where
-    world0 :: World
-    world0 = ()
+    world0 :: World2
+    world0 = World2 (C 0 1) R
 
-    stepTime :: Double -> World -> World
+    stepTime :: Double -> World2 -> World2
     stepTime _ = id
 
-    stepInput :: Event -> World -> World
-    stepInput _ = id
-
-    output :: World -> Picture
-    output _ = blank
+    output :: World2 -> Picture
+    output (World2 c d) = drawAt c (player2 d) & pictureOfMaze
 
 
 -- #4 NEW LEVELS
