e92f0157f9ac847c5835df5fe7aa56ca65bb56de
Ben Sinclair <ben.d.sinclair@gmail.com>
2016-11-25 00:27:05 +1100

    2016 - Week 4 - BenS

--- 2016/src/CIS194/Week4.hs
+++ 2016/src/CIS194/Week4.hs
@@ -3,12 +3,16 @@
 module CIS194.Week4 where
 
 import CodeWorld
+import Control.Applicative
+import Control.Monad (guard)
+import Control.Monad.Trans.Maybe
+import Data.Functor.Identity
 
 -- Coordinates
 
-data Coord = C Integer Integer
+data Coord = C Integer Integer deriving (Eq, Show)
 
-data Direction = R | U | L | D
+data Direction = R | U | L | D deriving (Eq, Show)
 
 eqCoord :: Coord -> Coord -> Bool
 eqCoord = undefined
@@ -42,29 +46,57 @@ combine Empty = blank
 combine (Entry p ps) = p & combine ps
 
 elemList :: Eq a => a -> List a -> Bool
-elemList = undefined
+elemList _ Empty = False
+elemList x (Entry y ys) = x == y || elemList x ys
 
 appendList :: List a -> List a -> List a
-appendList = undefined
+appendList Empty ys = ys
+appendList (Entry x xs) ys = Entry x (appendList xs ys)
+
+concatLists :: List (List a) -> List a
+concatLists Empty = Empty
+concatLists (Entry Empty ys) = concatLists ys
+concatLists (Entry (Entry x xs) ys) = Entry x (concatLists (Entry xs ys))
 
 listLength :: List a -> Integer
-listLength = undefined
+listLength Empty = 0
+listLength (Entry _ xs) = 1 + listLength xs
 
 filterList :: (a -> Bool) -> List a -> List a
-filterList = undefined
+filterList _ Empty = Empty
+filterList f (Entry x xs) =
+  if f x then Entry x (filterList f xs) else (filterList f xs)
 
 nth :: List a -> Integer -> a
-nth = undefined
+nth Empty _ = error "nth: list too short"
+nth (Entry x xs) i = case compare i 0 of
+  LT -> error "nth: i<0"
+  EQ -> x
+  GT -> nth xs (i - 1)
 
 -- Exercise 2: Graph Search
 
 isGraphClosed :: Eq a => a -> (a -> List a) -> (a -> Bool) -> Bool
-isGraphClosed initial adjacent isOk = undefined
+isGraphClosed initial adjacent isOk = go Empty (Entry initial Empty)
+  where
+    go _ Empty = True
+    go seen (Entry x xs)
+      | elemList x seen = go seen xs
+      | otherwise = isOk x && go (Entry x seen) (adjacent x)
 
 -- Exercise 3: Check closedness of mazes
 
 isClosed :: Maze -> Bool
-isClosed = undefined
+isClosed (Maze pos mz) = mz pos /= Box && isGraphClosed pos adjacent isOk
+  where
+    isOk x = mz x `elemList` Entry Ground (Entry Storage (Entry Box Empty))
+    adjacent x =
+      appendList (guardTile (adjacentCoord U x)) $
+      appendList (guardTile (adjacentCoord D x)) $
+      appendList (guardTile (adjacentCoord L x)) $
+                 (guardTile (adjacentCoord R x))
+    isWall t = elemList t (Entry Wall (Entry Blank Empty))
+    guardTile x = if isWall (mz x) then Empty else Entry x Empty
 
 pictureOfBools :: List Bool -> Picture
 pictureOfBools xs = translated (-fromIntegral k /2) (fromIntegral k) (go 0 xs)
@@ -83,14 +115,253 @@ pictureOfBools xs = translated (-fromIntegral k /2) (fromIntegral k) (go 0 xs)
         pictureOfBool False = colored red   (solidCircle 0.4)
 
 exercise3 :: IO ()
-exercise3 = undefined
+exercise3 = drawingOf (pictureOfBools (mapList isClosed extraMazes))
 
 -- Exercise 4: Multi-Level Sokoban
 -- Extend your game from last week (or the code from the lecture) to implement
 -- multi-level sokoban.
 
+-- The state
+
+data State = State
+  { playerPos  :: Coord
+  , playerDir  :: Direction
+  , gameBoxes  :: List Coord
+  } deriving Eq
+
+allCoords :: List Coord
+allCoords = go (-10) (-10)
+  where
+    go 11 10 = Empty
+    go 11 y = go (-10) (y+1)
+    go x y = Entry (C x y) (go (x+1) y)
+
+initialBoxes :: Maze -> List Coord
+initialBoxes (Maze _ mz) = concatLists (mapList (\c -> f (mz c) c) allCoords)
+  where
+    f t c = if t == Box then Entry c Empty else Empty
+
+initialState :: Maze -> State
+initialState mz = State (C 0 1) R (initialBoxes mz)
+
+noBoxMaze :: Maze -> Coord -> Tile
+noBoxMaze (Maze _ mz) c = if mz c == Box then Ground else mz c
+
+-- Event handling
+
+nextPos :: (Coord -> Tile) -> List Coord -> Direction -> Coord -> Maybe Coord
+nextPos mz boxes dir (C x y) =
+  pos <$ guard (mz pos `elem` [Ground, Storage]) <* guard (pos `notElem` boxes)
+  where
+    pos = case dir of
+      U -> C x (y+1)
+      D -> C x (y-1)
+      L -> C (x-1) y
+      R -> C (x+1) y
+
+shiftBoxes
+  :: (Coord -> Tile) -> Direction -> Coord -> List Coord -> Maybe (List Coord)
+shiftBoxes mz dir p boxes = go boxes
+  where
+    go Empty = Just Empty
+    go (Entry q cs)
+      | p == q = Entry <$> nextPos mz boxes dir q <*> go cs
+      | otherwise = Entry q <$> go cs
+
+handleEvent :: (Alternative m, Monad m) => Maze -> Event -> State -> m State
+handleEvent mz ev s@(State pos _ boxes) =
+  maybe (return s) (\d -> maybe (return (State pos d boxes)) return (go d)) dirMay
+  where
+    go d = do
+      p <- nextPos (noBoxMaze mz) Empty d pos <|> Just pos
+      boxes' <- shiftBoxes (noBoxMaze mz) d p boxes
+      return (State p d boxes')
+    dirMay = case ev of
+      KeyPress "Up"    -> Just U
+      KeyPress "Down"  -> Just D
+      KeyPress "Left"  -> Just L
+      KeyPress "Right" -> Just R
+      _                -> Nothing
+
+-- Drawing
+
+circ, square :: Color -> Picture
+circ col = coloured col (solidCircle 0.35)
+square col = coloured col (solidRectangle 1.02 1.02)
+
+drawTile :: Tile -> Picture
+drawTile tile =
+  case tile of
+    Wall    -> square (grey 0.4)
+    Ground  -> square yellow
+    Storage -> circ white & square yellow
+    Box     -> square brown
+    Blank   -> blank
+
+pictureOfMaze :: Maze -> Picture
+pictureOfMaze mz = draw21times (\r -> draw21times (\c -> drawTileAt mz (C r c)))
+
+draw21times :: (Integer -> Picture) -> Picture
+draw21times something = go (-10)
+  where
+    go :: Integer -> Picture
+    go 11 = blank
+    go n  = something n & go (n+1)
+
+drawTileAt :: Maze -> Coord -> Picture
+drawTileAt mz c = atCoord c (drawTile (noBoxMaze mz c))
+
+atCoord :: Coord -> Picture -> Picture
+atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
+
+player :: Direction -> Picture
+player dir =
+  rotated (case dir of L -> pi; R -> 0; U -> pi/2; D -> 3*pi/2)
+  $ translated 0.1 0.1 (scaled 0.2 0.2 (circ white))
+    & translated 0.1 (-0.1) (scaled 0.2 0.2 (circ white))
+    & circ black
+
+pictureOfBoxes :: List Coord -> Picture
+pictureOfBoxes cs = combine (mapList (\c -> atCoord c (drawTile Box)) cs)
+
+drawState :: Maze -> State -> Picture
+drawState mz (State pos dir boxes) =
+  atCoord pos (player dir) & pictureOfBoxes boxes & (pictureOfMaze mz)
+
+
+-- The complete interaction
+
+sokoban :: (Alternative m, Monad m) => Maze -> Interaction m State
+sokoban mz@(Maze _ f) =
+  Interaction (initialState mz) checkDone (handleEvent mz) (drawState mz)
+  where
+    checkDone _ s@(State _ _ boxes) =
+      s <$ guard (any (\x -> f x /= Storage) boxes)
+
+-- The general interaction type
+
+data Interaction m world = Interaction
+        world
+        (Double -> world -> m world)
+        (Event -> world -> m world)
+        (world -> Picture)
+
+runInteraction :: Interaction Identity s -> IO ()
+runInteraction (Interaction state0 step handle draw)
+  = interactionOf
+      state0
+      (\t st -> runIdentity (step t st))
+      (\e st -> runIdentity (handle e st))
+      draw
+
+-- Resetable interactions
+
+resetable :: Monad m => Interaction m s -> Interaction m s
+resetable (Interaction state0 step handle draw)
+  = Interaction state0 step handle' draw
+  where handle' (KeyPress key) _ | key == "Esc" = return state0
+        handle' e s = handle e s
+
+-- Levels
+
+data LState m world
+  = AllDone
+  | Levels
+      (List Maze)
+      world
+      (Double -> world -> MaybeT m world)
+      (Event -> world -> MaybeT m world)
+      (world -> Picture)
+
+withLevels
+  :: Monad m
+  => List Maze -> (Maze -> Interaction (MaybeT m) s) -> Interaction m (LState m s)
+withLevels allMazes f = case allMazes of
+  Empty ->
+    Interaction AllDone (const return) (const return) drawDone
+  Entry mz mzs -> case f mz of
+    Interaction s step handle draw ->
+      Interaction (Levels mzs s step handle draw) stepGo handleGo drawGo
+  where
+    drawDone _ = text "All Done!"
+
+    stepGo _ AllDone = return AllDone
+    stepGo t (Levels mzs s step handle draw) = do
+      sM' <- runMaybeT (step t s)
+      case sM' of
+        Just s' -> return (Levels mzs s' step handle draw)
+        Nothing -> nextLevel mzs
+
+    handleGo _ AllDone = return AllDone
+    handleGo e (Levels mzs s step handle draw) = do
+      sM' <- runMaybeT (handle e s)
+      case sM' of
+        Just s' -> return (Levels mzs s' step handle draw)
+        Nothing -> nextLevel mzs
+
+    drawGo AllDone = drawDone AllDone
+    drawGo (Levels _ s _ _ draw) = draw s
+
+    nextLevel mzs = case mzs of
+      Empty ->
+        return AllDone
+      Entry mz mzs' -> do
+        let Interaction s' fstep' fhandle' fdraw' = f mz
+        return (Levels mzs' s' fstep' fhandle' fdraw')
+
+-- Start screen
+
+startScreen :: Picture
+startScreen = scaled 3 3 (text "Sokoban!")
+
+data SSState world = StartScreen | Running world deriving Eq
+
+withStartScreen :: Monad m => Interaction m s  -> Interaction m (SSState s)
+withStartScreen (Interaction state0 step handle draw)
+  = Interaction state0' step' handle' draw'
+  where
+    state0' = StartScreen
+
+    step' _ StartScreen = return StartScreen
+    step' t (Running s) = Running <$> step t s
+
+    handle' (KeyPress key) StartScreen | key == " " = return (Running state0)
+    handle' _              StartScreen              = return StartScreen
+    handle' e              (Running s)              = Running <$> handle e s
+
+    draw' StartScreen = startScreen
+    draw' (Running s) = draw s
+
+-- Undoable interactions
+
+-- We need to remember the current state, and all past states:
+
+data WithUndo a = WithUndo a (List a)
+
+withUndo :: (Eq a, Monad m) => Interaction m a -> Interaction m (WithUndo a)
+withUndo (Interaction state0 step handle draw)
+  = Interaction state0' step' handle' draw'
+  where
+    draw' (WithUndo s _) = draw s
+
+    state0' = WithUndo state0 Empty
+
+    step' t (WithUndo s stack) = WithUndo <$> step t s <*> pure stack
+
+    handle' (KeyPress key) (WithUndo s stack) | key == "U" =
+      case stack of
+        Entry s' stack' -> return (WithUndo s' stack')
+        Empty -> return (WithUndo s Empty)
+    handle' e (WithUndo s stack) = do
+      s' <- handle e s
+      return (if s' == s then WithUndo s stack else WithUndo s' (Entry s stack))
+
 exercise4 :: IO ()
-exercise4 = undefined
+exercise4 =
+  runInteraction
+  . withStartScreen
+  . withLevels mazes
+  $ resetable . withUndo . sokoban
 
 main :: IO ()
 main = return ()
@@ -98,7 +369,7 @@ main = return ()
 
 -- MAZES
 
-data Tile = Wall | Ground | Storage | Box | Blank
+data Tile = Wall | Ground | Storage | Box | Blank deriving Eq
 
 data Maze = Maze Coord (Coord -> Tile)
 
