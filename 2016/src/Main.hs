66e0e3e2fdc5ced7029896d82b3b3b79126a0ba2
Ben Sinclair <ben.d.sinclair@gmail.com>
2017-01-02 21:56:03 +1100

    2016 - Week 7
    Skipping Week 6 since it has non-homework that we don't want to do.

--- 2016/src/Main.hs
+++ 2016/src/Main.hs
@@ -5,7 +5,8 @@ import qualified CIS194.Week2
 import qualified CIS194.Week3
 import qualified CIS194.Week4
 import qualified CIS194.Week5
+import qualified CIS194.Week7
 
 main :: IO ()
 main =
-  CIS194.Week5.main
+  CIS194.Week7.main
